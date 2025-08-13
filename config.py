a7082fc19273bbaabf17fde16e4c6f86d2440450
SPIDEY OFFICIAL <gaminghatyar777@gmail.com>
2025-08-13 05:58:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,8 @@ DB_NAME = os.environ.get("DATABASE_NAME", "FileHubxBot")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "10"))  # 0 means no expiry
 BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/Spideyofficial_777")
-TG_BOT_CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002423451263")WORKERS = int(os.environ.get("TG_BOT_WORKERS", "200"))
+TG_BOT_CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002423451263")
+WORKERS = int(os.environ.get("TG_BOT_WORKERS", "200"))
 #--------------------------------------------
 START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/ec17880d61180d3312d6a.jpg")
 FORCE_PIC = os.environ.get("FORCE_PIC", "https://telegra.ph/file/e292b12890b8b4b9dcbd1.jpg")
