693bcf0e61c8f30d9070aa81877df929afb23394
SPIDEY OFFICIAL <gaminghatyar777@gmail.com>
2025-08-13 13:08:31 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -306,5 +306,20 @@ class Spidey:
     async def is_user_exist(self, id):
         user = await self.col.find_one({'_id': int(id)})
         return bool(user)
+        
+def unpack_new_file_id(new_file_id):
+    """Return file_id, file_ref"""
+    decoded = FileId.decode(new_file_id)
+    file_id = encode_file_id(
+        pack(
+            "<iiqq",
+            int(decoded.file_type),
+            decoded.dc_id,
+            decoded.media_id,
+            decoded.access_hash
+        )
+    )
+    file_ref = encode_file_ref(decoded.file_reference)
+    return file_id, file_ref
                      
 db = Spidey(DB_URI, DB_NAME)
